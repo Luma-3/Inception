ecf4e0a57e76db3cdc4a75944d5dc7535b1f6296
Luma-3 <jb.brs@icloud.com>
2025-01-20 12:11:27 +0000

    re

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -2,17 +2,17 @@
 
 # Init Mariadb
 
+mysqld &
 
-# DB_ALREADY_EXISTS=`mysql -u root -e "SHOW DATABASES" | grep ${SQL_DATABASE}`
+DB_ALREADY_EXISTS=`mysql -u root -e "SHOW DATABASES" | grep ${SQL_DATABASE}`
 
-# if [ -n "$DB_ALREADY_EXISTS" ]; then
-# 	echo "Database already exists"
-# else
-# 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-# fi
+if [ -n "$DB_ALREADY_EXISTS" ]; then
+	echo "Database already exists"
+else
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+fi
 
 # service mysql start
-mysqld &
 
 while ! mysqladmin ping --silent; do
 	sleep 1
@@ -30,18 +30,4 @@ mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# service mysql stop
-
-# rc-service mariadb restart
-
 exec mysqld_safe
-
-# rc-service mariadb start
-
-# mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-
-# mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}'; GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%'; FLUSH PRIVILEGES;"
-
-# rc-service mariadb stop
-
-# exec "$@"
