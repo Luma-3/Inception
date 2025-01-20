4b3dcd5a2f01a5c9d9cf9a887cf04215fd822b3b
Luma-3 <jb.brs@icloud.com>
2025-01-20 18:59:12 +0000

    ca avance

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -8,9 +8,13 @@
 
 mv /tmp/wp-config.php /var/www/wordpress/wp-config.php
 
-# if [ ! -f /var/www/html/wp-config.php ]; then
-# 	mv /var/www/html/wp-config.php.tmp /var/www/html/wp-config.php
+# if [ ! -f /var/www/wordpress/wp-config.php ]; then
+# 	mv /tmp/wp-config.php /var/www/wordpress/wp-config.php
 # fi
+
+# /tmp/wp-cli.phar core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_AMDIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root
+
+# /tmp/wp-cli.phar user create $WP_USER2 $USER2_MAIL --user_pass=$USER2_PASS --allow-root
 
 exec "$@"
 
